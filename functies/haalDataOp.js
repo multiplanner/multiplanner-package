7ae862738ce2036282621bb378de4c0857b804fd
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-06 16:29:04 +0200

    fix ratelimiting

--- functies/haalDataOp.js
+++ functies/haalDataOp.js
@@ -3,22 +3,29 @@ const https = require('https');
 const readJSONSync = require('./readJSONSync.js');
 const config = readJSONSync("config");
 
-module.exports = (pad, apikey = config.ns_app_key_primary) => {
-    const options = {
-        host: 'gateway.apiportal.ns.nl',
-        path: pad,
-        headers: {
-            "Ocp-Apim-Subscription-Key": apikey
-        }
-    };
-
+const doeRequest = (opties) => {
     let antwoord = '';
     return new Promise ((resolve) => {
-            const request = https.request(options, (response) => {
+            const request = https.request(opties, (response) => {
                 response.on('data', (deel) => antwoord += deel);
-                response.on('end', () => resolve(JSON.parse(antwoord)));
+                response.on('end', () => {
+                    const resultaat = JSON.parse(antwoord);
+                    if (resultaat.statusCode == 429) {
+                        setTimeout(async () => resolve((await doeRequest(opties))), 100);
+                    } else {
+                        resolve(resultaat);
+                    }
+                });
             });
             request.end();
         }
     );
 };
+
+module.exports = async (pad, apikey = config.ns_app_key_primary) => await doeRequest({
+    host: 'gateway.apiportal.ns.nl',
+    path: pad,
+    headers: {
+        "Ocp-Apim-Subscription-Key": apikey
+    }
+});
