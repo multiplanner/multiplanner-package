57ffabc5d7051d17902119f9bfcc932ed5542711
Ard-Jan van Etten <ardjanve@gmail.com>
2021-12-31 06:12:04 +0100

    planReis af

--- functies/planReis.js
+++ functies/planReis.js
@@ -10,6 +10,7 @@ const {
     vertrekTijd,
     extractLeg
 } = require('./interpreters.js');
+const writeJSON = require('./writeJSON.js');
 
 const berekenWachttijden = (station) => {
     if (station.vertrek && station.wacht && !station.aankomst) station.aankomst = new Date(station.vertrek - - station.wacht);
@@ -21,49 +22,50 @@ const isCompleet = (station) => {
     return station.vertrek && station.aankomst;
 };
 
-const berekenTijdenVoorStation = async (reis, index) => {
+const berekenTijdenVoorStation = async (reis, index, legs) => {
     reis = reis.map(berekenWachttijden);
     const station = reis[index];
-
-    // console.log(station);
 
     // De vertrektijd van de vorige is bekend, dus de aankomsttijd van huidige kan worden berekend
     if (!station.aankomst && index > 0 && reis[index - 1].vertrek) {
         const vorige = reis[index - 1];
-        console.log(station, vorige);
-        const trip = await vroegsteVolledigeReis(vorige.station.code, station.station.code, vorige.vertrek, vorige.ritnummer);
+        const trip = await vroegsteVolledigeReis(vorige.code, station.code, vorige.vertrek, vorige.ritnummer);
         station.aankomst = aankomstTijd(trip);
-        console.log(station.aankomst);
-        
-        // console.log(trip);
-        // station.aankomst = 
+        legs.push(...trip.legs.map((leg, i) => extractLeg(leg, index * 1000 + i)));
     }
 
     // De aankomsttijd in de volgende is bekend, dus de vertrektijd van huidige kan worden berekend
     if (!station.vertrek && index < reis.length - 1 && reis[index + 1].aankomst) {
         const volgende = reis[index + 1];
-        // console.log(station, volgende);
-
+        const trip = await laatsteVolledigeReis(station.code, volgende.code, volgende.aankomst, station.ritnummer)
+        station.vertrek = vertrekTijd(trip);
+        legs.push(...trip.legs.map((leg, i) => extractLeg(leg, index * 1000 + i)));
     }
 };
 
 const planReis = async (reisplan) => {
     // verwerk ruwe invoer
     const reis = reisplan.reis;
+    const legs = [];
 
     for (const station of reis) {
         if (station.vertrek) station.vertrek = new Date(station.vertrek);
         if (station.aankomst) station.aankomst = new Date(station.aankomst);
-        station.station = zoekStation(station.station);
+        station.code = zoekStation(station.station).code;
     }
 
     if (!reis.some(station => station.vertrek || station.aankomst)) reisplan[0].vertrek = new Date();
 
-    // while (!reis.every(isCompleet)) {
-        Promise.all(reis
+    let i = 0;
+    while (!reis.every(isCompleet) && i++ < reis.length * 2 + 2) {
+        await Promise.all(reis
             .filter((station) => !isCompleet(station))
-            .map((_station, index) => berekenTijdenVoorStation(reis, index)));
-    // }
+            .map((_station, index) => berekenTijdenVoorStation(reis, index, legs)));
+    }
+
+    legs.sort((a, b) => a.index - b.index);
+
+    return legs;
 };
 
 module.exports = {
