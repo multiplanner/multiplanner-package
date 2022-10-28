9a00cdd3a5ea44ea682cee4160d01a2c66b5914d
Ard-Jan van Etten <ardjanve@gmail.com>
2022-10-28 15:33:45 +0200

    e

--- test/reset.js
+++
@@ -1,7 +0,0 @@
-import writeJSON from '../functies/writeJSON.js';
-
-(async () => {
-    await writeJSON({}, "spoorkaart");
-    await writeJSON([], "stations");
-    await writeJSON({}, "config");
-})();
